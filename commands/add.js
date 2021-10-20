b247f71646e9c4e594ed25d19bbbca6b37c19bc6
Higor <higormorenoribeiro@gmail.com>
2021-10-20 08:54:48 -0400

    Testing add feature

--- commands/add.js
+++ commands/add.js
@@ -8,26 +8,27 @@ const FriendsApi = new vrchat.FriendsApi(configuration);
 const UsersApi = new vrchat.UsersApi(configuration);
 
 module.exports = {
-	data: new SlashCommandBuilder()
-	.setName('add')
-	.setDescription('Request to add your VrChat user to friends list.')
+    data: new SlashCommandBuilder()
+    .setName('add')
+    .setDescription('Request to add your VrChat user to friends list.')
     .addStringOption(option => option.setName('name').setDescription('Enter your VrChat User Name')),
-	async execute(interaction) {
-        var result = '';
-		const name = interaction.options.getString('name');
-        AuthenticationApi.getCurrentUser().then(resp =>{
-            UsersApi.getUserByName(name).then(resp =>{
-                FriendsApi.friend(resp.data.id).then(resp =>{
-                    result = `Friend request sent to ${name} on VrChat.`
+    async execute(interaction) {
+        const name = ""+ interaction.options.getString('name');
+        if(name.includes(' ')){
+            interaction.reply(`Can't add users with spaces on name`);
+        }else{
+            AuthenticationApi.getCurrentUser().then(resp =>{
+                UsersApi.getUserByName(name).then(resp =>{
+                    FriendsApi.friend(resp.data.id).then(resp =>{
+                        interaction.reply(`Friend request sent to ${name} on VrChat.`);
+                    }).catch(error =>{
+                        interaction.reply('Error trying to add the user');
+                    });
                 }).catch(error =>{
-                    result = 'Error trying to add the user';
+                    interaction.reply('VrChat user not found.');
                 });
-            }).catch(error =>{
-                result = 'VrChat User not found';
             });
-        });
-
-        await interaction.reply(result);
-	},
+        }
+    },
 };
 
